2a2cf2fb5c9a96b0c5f754493de207ef7bd3c36c
lennykioko <lennykmutua@gmail.com>
2025-05-29 14:02:28 +0300

    Add main.py for testing

---
+++ main.py
@@ -0,0 +1,34 @@
+import datetime
+import time
+from PIL import ImageGrab
+import numpy as np
+import cv2
+from win32api import GetSystemMetrics
+
+width = GetSystemMetrics(0)
+height = GetSystemMetrics(1)
+
+time_stamp = datetime.datetime.now().strftime('%Y-%m-%d %H-%M-%S')
+file_name = f'{time_stamp}.mp4'
+fourcc = cv2.VideoWriter_fourcc(*'mp4v')  # or use 'XVID' and .avi if issues
+captured_video = cv2.VideoWriter(file_name, fourcc, 20.0, (width, height))
+
+try:
+    while True:
+        img = ImageGrab.grab(bbox=(0, 0, width, height))
+        img_np = np.array(img)
+        img_final = cv2.cvtColor(img_np, cv2.COLOR_RGB2BGR)
+        cv2.imshow('ScreenRecorder', img_final)
+
+        captured_video.write(img_final)
+        if cv2.waitKey(1) == ord('q'):
+            break
+        
+        time.sleep(1 / 20)  # Control FPS
+
+except KeyboardInterrupt:
+    print("Recording stopped manually.")
+
+finally:
+    captured_video.release()
+    cv2.destroyAllWindows()
